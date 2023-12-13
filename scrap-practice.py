e3250e968cd8757e5db8e8be6f297fa0f0ccc972
jschm404 <153659382+jschm404@users.noreply.github.com>
2023-12-13 10:06:55 +0000

    practice with regular expressions

--- scrap-practice.py
+++ scrap-practice.py
@@ -1,4 +1,7 @@
 from urllib.request import urlopen
+import re
+
+
 url = "http://olympus.realpython.org/profiles/aphrodite"
 page = urlopen(url)
     #urlopen() returns an HTTPResponse object
@@ -28,4 +31,34 @@ print(end_index)
 title = html[start_index:end_index]
 print(title)
     #Profile: Aphrodite
+
+
+#regular expressions - metacharacters denote different patters
+#for example, * stans for zero or more instances
+re.findall("ab*c", "ac")
+    #['ac']
+re.findall("ab*c", "abcd")
+    #['abc']
+re.findall("ab*c", "abcac")
+    #['abc', 'abcac']
+re.findall("ab*c", "abdc")
+    #[]
+#there is also ".", which stands for any since character in a regular expression
+re.findall("a.c", "abc")
+    #['abc']
+re.findall("a.c", "abbc")
+    #[]
+re.findall("a.c", "ac")
+    #[]
+re.findall("a.c", "acc")
+    #['acc']
+#they can be used together
+re.findall("a.*c", "abc")
+    #['abc']
+re.findall("a.*c", "abbc")
+    #['abbc']
+re.findall("a.*c", "ac")
+    #['ac']
+re.findall("a.*c", "acc")
+    #['acc']
 
