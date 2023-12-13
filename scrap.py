07cff6fe260b049c123697d63bd41c2605be0eca
jschm404 <153659382+jschm404@users.noreply.github.com>
2023-12-13 05:14:09 +0000

    it works

--- scrap.py
+++ scrap.py
@@ -1,1 +1,14 @@
+from urllib.request import urlopen
+url = "http://olympus.realpython.org/profiles/aphrodite"
+page = urlopen(url)
+    #urlopen() returns an HTTPResponse object
+page 
+    #<http.client.HTTPResponse object at 0x105fef820>
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+print(html)
+    #<html>
+    #<head>
+    #<title>Profile: Aphrodite</title>
+    #...
 
